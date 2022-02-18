58bfb590fafab26d6389ba19d780e0ba7cca309a
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2022-02-18 21:50:18 +0900

    db 연동

--- routes/test/test.js
+++ routes/test/test.js
@@ -3,16 +3,31 @@ const mysql = require("mysql");
 const db = require("../../db/index");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.set('port', 3000);
 
 app.get("/messages", (req, res) => {
-	db.connection.query(`SELECT * FROM testtable`, (err, results) => {
+	db.connection.query(`SELECT * FROM hello`, (err, results) => {
 		if (err)
 			console.log(err);
 		res.send(results);
 		console.log(results);
 	});
 });
+
+app.get("/insert", (req, res) => {
+	console.log(req.body);
+	var body = req.body;
+	var params = [body.id, body.name, body.pw, body.email, body.right];
+	var sql = `INSERT INTO user VALUES (?,?,?,?,?)`;
+	db.connection.query(sql,params,(err, results) => {
+		if (err) console.log(err);
+		else
+			res.send("okok");
+	})
+})
+//var sql = `INSERT INTO user VALUES ('${params[0]}','${params[1]}','${params[2]}','${params[3]}','${params[4]}')`;
 
 app.listen(app.get("port"));
 console.log("Listening on", app.get("port"));
