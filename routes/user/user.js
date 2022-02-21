4f1c1b950c70f39afc5b5f9337f01016c3449bde
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2022-02-21 22:56:57 +0900

    fix

--- routes/user/user.js
+++ routes/user/user.js
@@ -9,9 +9,8 @@ router.post('/signup', (req, res) => {
 	console.log(body);
 	const parms = [body.id, body.name, body.password, body.email, 0];
 	var sql = `INSERT INTO user VALUES (?,?,?,?,?)`;
-	db.connection.query(sql,parms, (err,results) =>
-	{
-		if(err) console.log(err);
+	db.connection.query(sql, parms, (err, results) => {
+		if (err) console.log(err);
 		else res.send(200);
 	})
 	console.log("User get!");
