0762ce76e92617bd044eedfa70487d662c3f7bb0
Chung Hyun Mok <hyunmok@Chungui-MacBookAir.local>
2022-02-22 17:30:23 +0900

    adsf

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,19 +1,18 @@
+const request = require('request');
 const express = require('express');
-const router = express.Router();
 const mysql = require("mysql");
 const db = require("../../db/index");
+const router = express.Router();
 
-
-router.post('/signup', (req, res) => {
-	const body = req.body;
-	console.log(body);
-	const parms = [body.id, body.name, body.password, body.email, 0];
-	var sql = `INSERT INTO user VALUES (?,?,?,?,?)`;
-	db.connection.query(sql, parms, (err, results) => {
-		if (err) console.log(err);
-		else res.send(200);
-	})
-	console.log("User get!");
-})
+router.post('/SignUp', (req, res) => {
+    var body = req.body;
+    console.log(body);
+    var params = [body.id, body.name, body.pw, body.email, 0];
+    var sql = 'INSERT INTO user VALUES (?, ?, ?, ?, ?)';
+    db.connection.query(sql, params, (err, results) => {
+        if(err) console.log(err);
+        console.log(results);
+    });
+});
 
 module.exports = router;
