5e979cf3a0a5cf34881f3f3f6ce4862261dba181
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2022-03-14 17:16:02 +0900

    add encrypt

--- routes/user/user.js
+++ routes/user/user.js
@@ -4,22 +4,28 @@ const mysql = require("mysql");
 const db = require("../../db/index");
 const router = express.Router();
 const user = require('./loginAction');
+const encoding = require('./encodingModule');
 
 router.post('/SignUp', (req, res) => {
     var body = req.body;
     console.log(body);
-    var params = [body.id, body.name, body.pw, body.email, 0];
-    var sql = 'INSERT INTO user VALUES (?, ?, ?, ?, ?)';
-    db.connection.query(sql, params, (err, results) => {
-        if(err) console.log(err);
-        console.log(results);
-    });
+    const hasedData = encoding.encryptPassword(body.pw)
+        .then((hasedData) => {
+            console.log(hasedData.hased);
+            var params = [body.id, body.name, hasedData.hashed, body.email, 0, hasedData.salt];
+            var sql = 'INSERT INTO user VALUES (?, ?, ?, ?, ?, ?)';
+            db.connection.query(sql, params, (err, results) => {
+                if (err) console.log(err);
+                console.log(results);
+            });
+        })
+    console.log(hasedData);
 });
 
 router.post('/Login', (req, res) => {
     var body = req.body;
     console.log(body);
-    loginSuccess = () => res.json({'hello' : 'asdf'});
+    loginSuccess = () => res.json({ 'result': 'true' });
     loginFalse = () => res.status(401).send({ 'result': 'false' });
     user.checkUser(body, loginSuccess, loginFalse);
 });
